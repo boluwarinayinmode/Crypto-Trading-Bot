d7208be6ab468a17c10fed20c78f1695bc5b29ad
Boluwarin <boluwarinayinmode@gmail.com>
2022-11-24 19:44:38 +0100

    Crypto Trading Bot First Push

---
+++ index.js
@@ -0,0 +1,4 @@
+const app = require('./src/app/index.js')
+const forever = require('forever')
+
+app.start()
---
+++ src/app/index.js
@@ -0,0 +1,8 @@
+const config = require('../configuration/index.js')
+
+module.exports = {
+  start: async () => {
+  }
+}
+
+//UTALPGAZCFO4Z3RXAWNXXIKFET4GEW
