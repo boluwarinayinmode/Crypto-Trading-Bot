136a07c741ebee4efc9ab03d3f373544a8dd7e6a
Boluwarin <boluwarinayinmode@gmail.com>
2022-11-25 09:32:12 +0100

    Keys

--- src/app/index.js
+++ src/app/index.js
@@ -1,5 +1,10 @@
+require('dotenv').config()
 const config = require('../configuration/index.js')
+const coinbase = require('coinbase');
 
+const apiKey=process.env.COINBASE_API_KEY
+const apiSecret=process.env.COINBASE_API_SECRET
+const client = new coinbase.Client({apiKey, apiSecret})
 module.exports = {
   start: async () => {
   }
